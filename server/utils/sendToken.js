36859803e309e55983a1f2f809cc117dee008a4a
Ayush Bhardvaj <ayushbhardvaj01@gmail.com>
2024-09-08 22:18:56 +0530

    cors debug

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,24 +1,24 @@
 const sendToken = (statusCode, user, res) => {
-    const token = user.getJWTToken();
-    const editedUser = user.toObject();
-    delete editedUser.password;
-    user = editedUser;
-  
-    //options for cookie
-    const options = {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      sameSite: "none",
-      secure: true,
-      httpOnly: true,
-    };
-    res.status(statusCode).cookie("token", token, options).json({
-      success: true,
-      user,
-      token,
-    });
+  const token = user.getJWTToken();
+  const editedUser = user.toObject();
+  delete editedUser.password;
+  user = editedUser;
+
+  //options for cookie
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    sameSite: "none",
+    secure: true,
+    httpOnly: true,
   };
-  
-  export default sendToken;
-  
+  res.getHeaders();
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    token,
+  });
+};
+
+export default sendToken;
